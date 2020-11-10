a5793ee3c2df90d7b9e5e09a72123b5dacecd9da
Salman Fariz Navas <salmanfan96@physik.fu-berlin.de>
2020-11-10 13:19:47 +0100

    funcs file changes (added more functions)

--- Code/funcs.py
+++ Code/funcs.py
@@ -41,3 +41,58 @@ def lj_force(pos,lj_params,shape_params,pbc_params):
                     force[j,k]=force[j,k]-(r[k]/rnorm)*f
 
     return(force)
+
+
+
+
+def posini(shape_params,pbc_params):
+    pos1=np.zeros((shape_params[0],shape_params[1]))
+    sep=np.zeros((shape_params[0]))
+    dimsep=np.zeros((shape_params[1]))
+    
+    for i in range(shape_params[0]):
+        print(i)
+        if i == 0:
+            for j in range(shape_params[1]):
+                pos1[i,j]=np.random.uniform(0.0,pbc_params[0])
+        else:
+            for j in range(shape_params[1]):
+                pos1[i,j]=np.random.uniform(0.0,pbc_params[0])
+            
+            for k in range(i):
+                if i!=k:
+                    for l in range(shape_params[1]):
+                        dimsep[l]=pos1[i,l]-pos1[k,l]
+                        if dimsep[l] > pbc_params[1]:
+                            dimsep[l]=dimsep[l]-pbc_params[0]
+                        if dimsep[l] <= -pbc_params[1]:
+                            dimsep[l]=dimsep[l]+pbc_params[0]
+                        
+                    #sep[k]=np.linalg.norm(dimsep)
+                    sep[k]=0
+                    for el in range(shape_params[1]):
+                        sep[k]=sep[k]+dimsep[el]**2
+                    
+                    sep[k]=np.sqrt(sep[k])
+                
+            while(any(q < 0.6 and q!=0.0 for q in sep)):
+                for j in range(shape_params[1]):
+                    pos1[i,j]=np.random.uniform(0.0,pbc_params[0])
+                for k in range(i):
+                    if i!=k:
+                        for l in range(shape_params[1]):
+                            dimsep[l]=np.mod(pos1[i,l]-pos1[k,l],pbc_params[0])
+                            if dimsep[l] > pbc_params[1]:
+                                dimsep[l]=dimsep[l]-pbc_params[0]
+                                
+                            if dimsep[l] <= -pbc_params[1]:
+                                dimsep[l]=dimsep[l]+pbc_params[0]
+                            
+                        #sep[k]=np.linalg.norm(dimsep)
+                        sep[k]=0
+                        for el in range(shape_params[1]):
+                            sep[k]=sep[k]+dimsep[el]**2
+                        sep[k]=np.sqrt(sep[k])
+    
+    return(pos1)
+                        
